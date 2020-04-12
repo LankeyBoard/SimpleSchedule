3464fe466be59bc0fa834f6e99cb673fac86e592
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2020-04-11 22:19:45 -0400

    Calendar now displays

---
+++ employee/src/components/calendar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+const localizer = momentLocalizer(moment);
+
+const Cal = ({events}) => {
+  return (
+    <div className="Calendar">
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={events}
+        style={{ height: "100vh" }}
+      />
+    </div>
+  );
+}
+
+
+export default Cal;
--- employee/src/pages/HomePage.js
+++ employee/src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Cal from '../components/calendar';
+import events from '../components/calendar-setup'
 
 const HomePage = () => (
     <>
     <h1>Home Page</h1>
     <p>This is where the schedule should be displayed</p>
+    <div className="focus">
+    <Cal events = {events}/>
+    </div>
     </>
 )
 export default HomePage;
