15a818e69a0103594fa32d58558ea63c11dd77e3
Destiny-16 <destiny6@mail.usf.edu>
2020-04-14 15:45:55 -0400

    Update EmployeeInfo.js
    Form with light blue background and submit button

--- employee/src/pages/EmployeeInfo.js
+++ employee/src/pages/EmployeeInfo.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
-const Info = () => (
+const TimeOff = () => (
     <>
-    <h1>Employee Info</h1>
-    <p>This will be the epmloyees basic info, pay rate, etc </p>
+    <form>
+        <h1>Time Off Request Form</h1>
+        <br></br>
+        <div style={{backgroundColor: "lightblue"}}>
+            <p>Name: </p>
+            <input
+                type="text"
+            />
+            <br></br>
+            <br></br>
+            <p>Employee ID: </p>
+            <input
+                type="text"
+            />
+            <br></br>
+            <br></br>
+            <p>Leaving Date: </p>
+            <input
+                type="text"
+            />
+            <br></br>
+            <br></br>
+            <p>Returning Date: </p>
+            <input
+                type="text"
+            />
+            <br></br>
+            <br></br>
+        </div> 
+        <button>Submit</button>   
+    </form>
     </>
 )
-export default Info;
+export default TimeOff;
