d504c8099934e60c4a52b7db2233d4cea992f071
Bryan Lopez <bryanlopezrr@gmail.com>
2020-04-19 23:05:17 -0400

    availability branch with a modal

--- employee/src/components/Employee-Info.js
+++ employee/src/components/Employee-Info.js
@@ -1,6 +1,7 @@
 import React from 'react';
+
 export default () => <div id="left_sidebar">
-        <h2>Emplyee Info</h2>
+        <h2>Employee Info</h2>
         <p>Photo</p>
         <p>Name</p>
         <p>More info ... total hours, areas allowed to work, etc.</p>
---
+++ employee/src/components/modal/modal.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import './modal.css';
+import DatePicker from 'react-datepicker'
+
+const Modal = (props) => {
+
+    const [startDate, setStartDate] = useState(new Date());
+
+    return (
+        <div>
+            <div className="modal-wrapper"
+                style={{
+                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                <div className="modal-header">
+                    Availability Form
+                </div>
+                <div className="modal-body">
+                    <p>
+                        {props.children}
+                        <form>
+                            <div>When is this availability starting?</div>
+                            <DatePicker
+                                todayButton="Today"
+                                selected={startDate}
+                                onChange={date => setStartDate(date)}
+                            />
+                            <div >What shift would you rather work:</div>
+                            <label id="morning_lbl">Morning
+                                <input type="radio" id="morning"></input>
+                            </label>
+                            <label id="everning_lbl">Evening
+                                <input type="radio" id="evening"></input>
+                            </label>
+                            <div>What are your preffered days off:</div>
+                            <label>Sunday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Monday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Tuesday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Wednesday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Thursday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Friday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Saturday
+                                <input type="checkbox"></input>
+                            </label>
+                            <div>What are your preffered days to work:</div>
+                            <label>Sunday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Monday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Tuesday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Wednesday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Thursday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Friday
+                                <input type="checkbox"></input>
+                            </label>
+                            <label>Saturday
+                                <input type="checkbox"></input>
+                            </label>
+                        </form>
+                    </p>
+                    <button className="btn-cancel" onClick={props.handleClose}>Cancel</button>      
+                    <button className="btn-continue">Save</button>  
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+/*
+    The modal should have: 
+    Days you prefer to work - have off 
+    |   M   | T    |   W   | TH   |  F   |  S   |  SU  |
+
+    What times of the day would you rather work?
+    Evening - Morning 
+
+    Beginning date of availability 
+
+*/
+export default Modal;
