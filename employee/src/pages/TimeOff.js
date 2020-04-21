4dae7b21fe07dce1106362a0eda5fcf1a4f8ee2a
Destiny-16 <destiny6@mail.usf.edu>
2020-04-20 20:47:50 -0400

    Update TimeOff.js
    Allows employees to select times/days on calendar that they would like to request off; prompts for employees name and reason then saves to calendar

--- employee/src/pages/TimeOff.js
+++ employee/src/pages/TimeOff.js
@@ -1,5 +1,56 @@
-import React from 'react';
-export default () => <>
-        <h1>Time Off Request Form</h1>
-        <p>A time off request form will display below</p>
-    </>
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import ScheduleDetail from'../components/Schedule-Details';
+import moment from "moment";
+
+const events = [{
+  EmployeeName: '',
+  Reason: ''
+}];
+const localizer = momentLocalizer(moment);  
+
+class TimeOff extends React.Component {
+    constructor(...args) {
+      super(...args)
+  
+      this.state = { events }
+    }
+  
+    handleSelect = ({ start, end}) => {
+      const title = window.prompt('Employee Name:')
+      const title2 = window.prompt('Reason for Time Off:')
+      if (title)
+        this.setState({
+          events: [
+            ...this.state.events,
+            {
+              start,
+              end,
+              title,
+              title2
+            },
+          ],
+        })
+    }
+
+    render() {
+        const localizer = momentLocalizer(moment);
+        return (
+            <div className="Calendar" id="time_off">
+                <Calendar
+                selectable
+                localizer={localizer}
+                defaultDate={new Date()}
+                defaultView="month"
+                events={this.state.events}
+                onSelectEvent={event=> alert(event.title)}
+                onSelectEvent={event=> alert(event.title2)}
+                onSelectSlot={this.handleSelect}
+                defaultView="week"
+                />
+            </div>            
+        );
+    }
+}
+
+export default TimeOff
