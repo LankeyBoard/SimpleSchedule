a07171b7c35f4f82782cd9f15a9bb4b262fc1d26
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2020-04-22 17:50:15 -0400

    Shedule details now dynamic on the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -66,7 +66,7 @@ export default (props) => {
                     />
                 </div>
             </div>
-            <ScheduleDetail />
+            <ScheduleDetail shifts={events} />
         </div>
     )
 }
