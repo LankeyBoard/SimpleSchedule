daa86df644222c5124d6152b644553eaec0c23e7
Felipe Ferreira <felipedferreira11@gmail.com>
2020-04-22 18:27:00 -0400

    wiring up the request time off page

--- server/routes/event.js
+++ server/routes/event.js
@@ -76,5 +76,54 @@ eventController.get('/getAllEvents', async (req, res) => {
     }
 })
 
+// @route       Post /api/events/timeOffRequest
+// @desc        Used to make a request for timeOff
+// @access      Public
+eventController.post('/timeOffRequest', [
+    check('start', 'start is required').exists(),
+    check('end', 'end is required').exists(),
+    check('title', "title is required").exists(),
+    check('allDay', 'allDay is required').exists(),
+    check('userObjectId', "userObjectId is required").exists(),
+], async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })//bad request
+        }
+        // extracting these properties from the body...
+        const { start, end, title, allDay, userObjectId } = req.body
+        const _event = new Event({
+            title,
+            start: new Date(start), 
+            end: new Date(end), 
+            allDay,
+            userObjectId,
+            status: 'pending',
+            description: 'time off request',
+            isTimeOff: true
+        })
+        await _event.save()
+        res.send({ isSuccess: true, eventDetails: _event })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send('Server error')
+    }
+})
+
+
+// @route       Get /api/events/timeOffRequestRead
+// @desc        Used to make a request for timeOff
+// @access      Public
+eventController.get('/timeOffRequestRead', async (req, res) => {
+    try {
+        const timeOffEvents = await Event.find({isTimeOff: true})
+        res.send({ isSuccess: true, timeOffEvents })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send('Server error')
+    }
+})
+
 
 module.exports = eventController
--- src/pages/TimeOff.js
+++ src/pages/TimeOff.js
@@ -1,55 +1,83 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import ScheduleDetail from'../components/Schedule-Details';
+// import ScheduleDetail from'../components/Schedule-Details';
+import { AppContext } from './../App'
 import moment from "moment";
+import Axios from 'axios';
 
-const events = [{
-  Reason: ''
-}];
+const TimeOff = () => {
+  const { userData } = useContext(AppContext)
+  const [events, setEvents] = useState([])
 
-const localizer = momentLocalizer(moment);
-
-class TimeOff extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { events }
-    }
-
-    componentDidMount(){
-      console.dir(this.props)
-    }
+  useEffect(() => {
+    Axios.get('/api/events/timeOffRequestRead').then((axiosResponse) => {
+      const _events = axiosResponse.data.timeOffEvents.map(e => {
+        return {
+          ...e,
+          start:  moment(e.start).toDate(),
+          end:    moment(e.end).toDate()
+        }
+      })
+      setEvents(_events)
+    }).catch((apiError) => {
+      console.dir(apiError)
+    })
+  }, [])
   
-    handleSelect = ({ start, end}) => {
-      const title = window.prompt('Reason for Time Off:')
-      if (title)
-        this.setState({
-          events: [
-            ...this.state.events,
-            {
-              start,
-              end,
-              title
-            },
-          ],
-        })
-    }
+  const handleSelect = ({ start, end}) => {
+    console.dir(userData)
+    const title = window.prompt('Reason for Time Off:');
+    if (title) 
+    {
+     const newTimeOffEvent = {
+       start: moment(start).toDate(),
+       end: moment(end).toDate(),
+       title,
+       userObjectId: userData._id,
+       allDay: false
+     }
+     newTimeOffEvent.allDay = newTimeOffEvent.start.getHours() === newTimeOffEvent.end.getHours()
 
-    render() {
-        const localizer = momentLocalizer(moment);
-        return (
-            <div className="Calendar" id="time_off">
-                <Calendar
-                selectable
-                localizer={localizer}
-                defaultDate={new Date()}
-                defaultView="month"
-                events={this.state.events}
-                onSelectSlot={this.handleSelect}
-                defaultView="week"
-                />
-            </div>            
-        );
+      const newEvents = [...events,newTimeOffEvent]
+      setEvents(newEvents)
+
+      Axios.post('/api/events/timeOffRequest', newTimeOffEvent).then((axiosResponse) => {
+        debugger
+        console.dir(axiosResponse)
+      }).catch((apiError) => {
+        console.dir(apiError)
+      })
+
+
+
     }
+  }
+
+
+  // no logic for this as they are all timeOff
+  const eventPropGetter = () => {
+    return {
+        style: {
+            backgroundColor: 'crimson',
+            color: 'white',
+        }
+    }
+  }
+
+    const localizer = momentLocalizer(moment);
+    
+    // defaultView="month"
+    return <div className="Calendar" id="time_off">
+            <Calendar
+              selectable
+              localizer={localizer}
+              defaultDate={new Date()}
+              events={events}
+              onSelectSlot={handleSelect}
+              defaultView="week"
+              eventPropGetter={eventPropGetter}
+            />
+        </div>
 }
 
 export default TimeOff
