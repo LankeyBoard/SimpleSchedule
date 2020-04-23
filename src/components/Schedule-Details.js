b3f34c0a066f42dc7c4ff0df84009c3252059392
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2020-04-23 17:03:28 -0400

    tracks total hours scheduled

--- src/components/Schedule-Details.js
+++ src/components/Schedule-Details.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Shift from './Schedule-Event'
 //import shifts from './../shifts'// should NOT be hard coded...
+function TotalHours(shifts){
+    let totalHrs = 0;
+    for(let i = 0; i<shifts.length; i++){
+        totalHrs+= shifts[i].end.getHours()-shifts[i].start.getHours();
+    }
+    return totalHrs;
+}
 export default ({shifts}) => <div id='right_sidebar'>
         <h2>Schedule Details</h2>
-
+        <p>Hours: {TotalHours(shifts)}</p>
         <p>Shift Details</p>
         {shifts.map(
             (shift, i) => 
                 <Shift 
                     key={i}
                     title={shift.title} 
-                    startTime={shift.startTime}
-                    endTime={shift.endTime}
+                    startTime={shift.start}
+                    endTime={shift.end}
                     // startDate={shift.startDate}
                     // endDate={shift.endDate}
                     desc={shift.desc}
